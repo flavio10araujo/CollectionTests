5eecc97095706c437015776c963fd8fa0fa3cf30
flavio10araujo <flavio10araujo@yahoo.com.br>
2021-07-13 13:31:13 -0400

    OK

--- src/com/tests/collection/queue/deque/arraydeque/AMain.java
+++ src/com/tests/collection/queue/deque/arraydeque/AMain.java
@@ -4,26 +4,45 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class AMain {
-	
-	public static void main(String[] args) {
-		
-		// Create a Deque and use it as stack.
-		Deque<String> deque = new ArrayDeque<>();
-		
-		deque.push("Oracle");
-		deque.push("HTML");
-		deque.push("CSS");
-		deque.push("XML");
 
-		System.out.println("Stack: " + deque); // Stack: [XML, CSS, HTML, Oracle]
+	public static void main(String[] args) {
+
+		// Create a Deque and use it as QUEUE.
+		Deque<String> queue = new ArrayDeque<>();
+
+		queue.add("Oracle");
+		queue.add("HTML");
+		queue.add("CSS");
+		queue.add("XML");
+
+		System.out.println("Queue: " + queue); // Stack: [XML, CSS, HTML, Oracle]
 
 		// Remove all elements from the Deque.
-		while (deque.peek() != null) {
-			System.out.println("Element at top: " + deque.peek());
-			System.out.println("Popped: " + deque.pop());
-			System.out.println("Stack: " + deque);
+		while (queue.peek() != null) {
+			System.out.println("Element at top: " + queue.peek());
+			System.out.println("Popped: " + queue.pop());
+			System.out.println("Queue: " + queue);
 		}
 
-		System.out.println("Stack is empty: " + deque.isEmpty());
+		System.out.println("Queue is empty: " + queue.isEmpty());
+
+		// Create a Deque and use it as STACK.
+		Deque<String> stack = new ArrayDeque<>();
+
+		stack.push("Oracle");
+		stack.push("HTML");
+		stack.push("CSS");
+		stack.push("XML");
+
+		System.out.println("Stack: " + stack); // Stack: [XML, CSS, HTML, Oracle]
+
+		// Remove all elements from the Deque.
+		while (stack.peek() != null) {
+			System.out.println("Element at top: " + stack.peek());
+			System.out.println("Popped: " + stack.pop());
+			System.out.println("Stack: " + stack);
+		}
+
+		System.out.println("Stack is empty: " + stack.isEmpty());
 	}
 }
